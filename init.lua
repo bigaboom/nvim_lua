5b90295a0021cefd9f133e8c90a30d8c18383a26
Афанасьев Сергей <index2.php@gmail.com>
2021-08-10 09:29:20 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,251 @@
+-- neovim config
+-- github.com/bigaboom
+
+-------------------- HELPERS -------------------------------
+local api, cmd, fn, g = vim.api, vim.cmd, vim.fn, vim.g
+local opt, wo = vim.opt, vim.wo
+local fmt = string.format
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-------------------- PLUGINS -------------------------------
+cmd 'packadd paq-nvim'
+local paq = require('paq-nvim').paq
+paq {'airblade/vim-rooter'}
+paq {'hrsh7th/nvim-compe'}
+paq {'joshdick/onedark.vim'}
+paq {'junegunn/fzf'}
+paq {'junegunn/fzf.vim'}
+paq {'justinmk/vim-dirvish'}
+paq {'lervag/vimtex'}
+paq {'lewis6991/gitsigns.nvim'}
+paq {'lukas-reineke/indent-blankline.nvim'}
+paq {'machakann/vim-sandwich'}
+paq {'neovim/nvim-lspconfig'}
+paq {'nvim-lua/plenary.nvim'}
+paq {'nvim-treesitter/nvim-treesitter'}
+paq {'nvim-treesitter/nvim-treesitter-textobjects'}
+paq {'ojroques/nvim-bufbar'}
+paq {'ojroques/nvim-bufdel'}
+paq {'ojroques/nvim-buildme'}
+paq {'ojroques/nvim-hardline'}
+paq {'ojroques/nvim-lspfuzzy'}
+paq {'ojroques/vim-oscyank'}
+paq {'savq/paq-nvim', opt = true}
+paq {'terrortylor/nvim-comment'}
+paq {'tpope/vim-fugitive'}
+paq {'tpope/vim-unimpaired'}
+paq {'jiangmiao/auto-pairs'}
+paq {'npxbr/gruvbox.nvim'}
+paq {'scrooloose/nerdtree'}
+paq {'tomtom/tcomment_vim'}
+
+-------------------- PLUGIN SETUP --------------------------
+-- bufbar
+require('bufbar').setup {show_bufname = 'all', show_flags = true}
+-- bufdel
+require('bufdel').setup {next = 'alternate', quit = false}
+map('n', '<leader>w', '<cmd>BufDel<CR>')
+-- buildme
+map('n', '<leader>bb', '<cmd>BuildMe<CR>')
+map('n', '<A-1>', '<cmd>b1<CR>')
+map('n', '<A-2>', '<cmd>b2<CR>')
+map('n', '<A-3>', '<cmd>b3<CR>')
+map('n', '<A-4>', '<cmd>b4<CR>')
+map('n', '<A-5>', '<cmd>b5<CR>')
+map('n', '<A-6>', '<cmd>b6<CR>')
+map('n', '<A-7>', '<cmd>b7<CR>')
+map('n', '<A-8>', '<cmd>b8<CR>')
+map('n', '<A-9>', '<cmd>b9<CR>')
+map('n', '<A-0>', '<cmd>b10<CR>')
+map('n', '<A-,>', '<cmd>bprev<CR>')
+map('n', '<A-.>', '<cmd>bnext<CR>')
+map('n', '<leader>be', '<cmd>BuildMeEdit<CR>')
+map('n', '<leader>bs', '<cmd>BuildMeStop<CR>')
+-- comment
+require('nvim_comment').setup {}
+-- compe
+require('compe').setup {
+  min_length = 2,
+  preselect = 'disable',
+  max_abbr_width = 80, max_kind_width = 40, max_menu_width = 40,
+  source = {buffer = true, path = true, nvim_lsp = true, omni = {filetypes = {'tex'}}},
+}
+-- dirvish
+g['dirvish_mode'] = [[:sort ,^.*[\/],]]
+-- fugitive and git
+local log = [[\%C(yellow)\%h\%Cred\%d \%Creset\%s \%Cgreen(\%ar) \%Cblue\%an\%Creset]]
+map('n', '<leader>g<space>', ':Git ')
+map('n', '<leader>gd', '<cmd>Gvdiffsplit<CR>')
+map('n', '<leader>gg', '<cmd>Git<CR>')
+map('n', '<leader>gl', fmt('<cmd>term git log --graph --all --format="%s"<CR><cmd>start<CR>', log))
+-- fzf
+g['fzf_action'] = {['ctrl-s'] = 'split', ['ctrl-v'] = 'vsplit'}
+map('n', '<leader>/', '<cmd>BLines<CR>')
+map('n', '<leader>f', '<cmd>Files<CR>')
+map('n', '<leader>;', '<cmd>History:<CR>')
+map('n', '<leader>r', '<cmd>Rg<CR>')
+map('n', 's', '<cmd>Buffers<CR>')
+-- gitsigns
+require('gitsigns').setup {
+  signs = {
+    add = {text = '+'},
+    change = {text = '~'},
+    delete = {text = '-'}, topdelete = {text = '-'}, changedelete = {text = '≃'},
+  },
+}
+-- hardline
+require('hardline').setup {}
+-- indent-blankline
+g['indent_blankline_char'] = '┊'
+g['indent_blankline_buftype_exclude'] = {'terminal'}
+g['indent_blankline_filetype_exclude'] = {'fugitive', 'fzf', 'help', 'man'}
+-- lspfuzzy
+require('lspfuzzy').setup {}
+-- NERDTree
+map('n', '<Leader><TAB>', '<cmd>NERDTreeToggle<CR>')
+-- vim-sandwich
+cmd 'runtime macros/sandwich/keymap/surround.vim'
+-- vimtex
+g['vimtex_quickfix_mode'] = false
+
+-------------------- OPTIONS -------------------------------
+local indent, width = 2, 90
+opt.colorcolumn = tostring(width)   -- Line length marker
+opt.completeopt = {'menuone', 'noinsert', 'noselect'}  -- Completion options
+opt.cursorline = true               -- Highlight cursor line
+opt.expandtab = true                -- Use spaces instead of tabs
+opt.formatoptions = 'crqnj'         -- Automatic formatting options
+opt.hidden = true                   -- Enable background buffers
+opt.ignorecase = true               -- Ignore case
+opt.joinspaces = false              -- No double spaces with join
+opt.list = true                     -- Show some invisible characters
+opt.number = true                   -- Show line numbers
+opt.pastetoggle = '<F2>'            -- Paste mode
+opt.pumheight = 12                  -- Max height of popup menu
+opt.relativenumber = false          -- Relative line numbers
+opt.scrolloff = 4                   -- Lines of context
+opt.shiftround = true               -- Round indent
+opt.shiftwidth = indent             -- Size of an indent
+opt.shortmess = 'atToOFc'           -- Prompt message options
+opt.sidescrolloff = 8               -- Columns of context
+opt.signcolumn = 'yes'              -- Show sign column
+opt.smartcase = true                -- Do not ignore case with capitals
+opt.smartindent = true              -- Insert indents automatically
+opt.splitbelow = true               -- Put new windows below current
+opt.splitright = true               -- Put new windows right of current
+opt.tabstop = indent                -- Number of spaces tabs count for
+opt.termguicolors = true            -- True color support
+opt.textwidth = width               -- Maximum width of text
+opt.updatetime = 100                -- Delay before swap file is saved
+opt.wildmode = {'list', 'longest'}  -- Command-line completion mode
+opt.wrap = false                    -- Disable line wrap
+opt.mouse = "a"
+cmd 'colorscheme onedark'
+
+-------------------- MAPPINGS ------------------------------
+map('', '<leader>c', '"+y')
+map('i', '<C-u>', '<C-g>u<C-u>')
+map('i', '<C-w>', '<C-g>u<C-w>')
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+map('i', 'jj', '<ESC>')
+map('n', '<C-l>', '<cmd>nohlsearch<CR>')
+map('n', '<C-w>T', '<cmd>tabclose<CR>')
+map('n', '<C-w>t', '<cmd>tabnew<CR>')
+map('n', '<F3>', '<cmd>lua toggle_wrap()<CR>')
+map('n', '<F4>', '<cmd>set scrollbind!<CR>')
+map('n', '<F5>', '<cmd>checktime<CR>')
+map('n', '<S-Down>', '<C-w>2<')
+map('n', '<S-Left>', '<C-w>2-')
+map('n', '<S-Right>', '<C-w>2+')
+map('n', '<S-Up>', '<C-w>2>')
+map('n', '<leader>s', ':%s//gcI<Left><Left><Left><Left>')
+map('n', '<leader>t', '<cmd>terminal<CR>')
+map('n', '<leader>u', '<cmd>update<CR>')
+map('n', '<leader>x', '<cmd>conf qa<CR>')
+map('n', 'Q', '<cmd>lua warn_caps()<CR>')
+map('n', 'U', '<cmd>lua warn_caps()<CR>')
+map('t', '<ESC>', '&filetype == "fzf" ? "\\<ESC>" : "\\<C-\\>\\<C-n>"' , {expr = true})
+map('t', 'jj', '<ESC>', {noremap = false})
+map('v', '<leader>s', ':s//gcI<Left><Left><Left><Left>')
+
+-------------------- LSP -----------------------------------
+for ls, cfg in pairs({
+  bashls = {}, gopls = {}, ccls = {}, jsonls = {}, pylsp = {},
+}) do require('lspconfig')[ls].setup(cfg) end
+map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+
+-------------------- TREE-SITTER ---------------------------
+require('nvim-treesitter.configs').setup {
+  ensure_installed = 'maintained',
+  highlight = {enable = true},
+  textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        ['aa'] = '@parameter.outer', ['ia'] = '@parameter.inner',
+        ['af'] = '@function.outer', ['if'] = '@function.inner',
+        ['ac'] = '@class.outer', ['ic'] = '@class.inner',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {['<leader>a'] = '@parameter.inner'},
+      swap_previous = {['<leader>A'] = '@parameter.inner'},
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {[']a'] = '@parameter.outer', [']f'] = '@function.outer', [']c'] = '@class.outer'},
+      goto_next_end = {[']A'] = '@parameter.outer', [']F'] = '@function.outer', [']C'] = '@class.outer'},
+      goto_previous_start = {['[a'] = '@parameter.outer', ['[f'] = '@function.outer', ['[c'] = '@class.outer'},
+      goto_previous_end = {['[A'] = '@parameter.outer', ['[F'] = '@function.outer', ['[C'] = '@class.outer'},
+    },
+  },
+}
+
+-------------------- NERDTree ------------------------------
+g['NERDTreeShowHidden'] = 1
+g['NERDTreeMinimalUI'] = 1
+g['NERDTreeStatusLine'] = ''
+
+g['NERDCreateDefaultMappings'] = 0
+g['NERDSpaceDelims'] = 1
+
+-------------------- COMMANDS ------------------------------
+function init_term()
+  cmd 'setlocal nonumber norelativenumber'
+  cmd 'setlocal nospell'
+  cmd 'setlocal signcolumn=auto'
+end
+
+function toggle_wrap()
+  wo.breakindent = not wo.breakindent
+  wo.linebreak = not wo.linebreak
+  wo.wrap = not wo.wrap
+end
+
+function warn_caps()
+  cmd 'echohl WarningMsg'
+  cmd 'echo "Caps Lock may be on"'
+  cmd 'echohl None'
+end
+
+vim.tbl_map(function(c) cmd(fmt('autocmd %s', c)) end, {
+  'TermOpen * lua init_term()',
+  'TextYankPost * lua vim.highlight.on_yank {timeout = 200, on_visual = false}',
+  'TextYankPost * if v:event.operator is "y" && v:event.regname is "+" | OSCYankReg + | endif',
+})
